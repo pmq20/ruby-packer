0fd5d20fad389266554381204fe2c3394ca4cee5
Minqi Pan <pmq2001@gmail.com>
2017-06-13 17:23:50 +0800

    relax Ruby version requirement

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -286,9 +286,11 @@ class Compiler
     expectation = "ruby #{self.class.ruby_version}"
     got = `ruby -v`
     unless got.include?(expectation)
-      msg  = "Please make sure to have installed the correct version of ruby in your environment\n"
+      msg =  "=== WARNING ==="
+      msg += "Please make sure to have installed the correct version of ruby in your environment\n"
+      msg += "It should match the enclosed Ruby runtime version of the compiler.\n"
       msg += "Expecting #{expectation}; yet got #{got}"
-      raise Error, msg
+      STDERR.puts msg
     end
   end
   
