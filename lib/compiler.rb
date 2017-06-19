b9e56724733792b053d0a47d55b4386e80e4b815
Minqi Pan <pmq2001@gmail.com>
2017-06-19 17:13:42 +0800

    allow 1st nmake to fail

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -316,7 +316,7 @@ class Compiler
                                   --enable-debug-env \
                                   --disable-install-doc \
                                   --with-static-linked-ext")
-          Utils.run(@compile_env, "nmake #{@options[:nmake_args]}")
+          Utils.run_allow_failures(@compile_env, "nmake #{@options[:nmake_args]}")
           Utils.run(@compile_env, %Q{nmake #{@options[:nmake_args]} -f enc.mk V="0" UNICODE_HDR_DIR="./enc/unicode/9.0.0"  RUBY=".\\miniruby.exe -I./lib -I. " MINIRUBY=".\\miniruby.exe -I./lib -I. " -l libenc})
           Utils.run(@compile_env, %Q{nmake #{@options[:nmake_args]} -f enc.mk V="0" UNICODE_HDR_DIR="./enc/unicode/9.0.0"  RUBY=".\\miniruby.exe -I./lib -I. " MINIRUBY=".\\miniruby.exe -I./lib -I. " -l libtrans})
           Utils.run(@compile_env, "nmake #{@options[:nmake_args]}")
