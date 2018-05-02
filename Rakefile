a6c9a8cd42347fa1eb42a1f3c60b513a08402b18
Eric Hodel <drbrain@segment7.net>
2018-05-02 14:39:32 -0700

    Run our own tests from our built rubyc to verify
    This will replace tests/ruby-compiler by running our own tests rather
    than ruby's test suite.

--- Rakefile
+++ Rakefile
@@ -11,10 +11,29 @@ rubyc_deps = FileList[
   "vendor/**/*",
 ]
 
-file 'rubyc' => rubyc_deps do
+file "rubyc" => rubyc_deps do
   ruby "bin/rubyc", "bin/rubyc", "-o", "rubyc"
 end
 
+namespace "rubyc" do
+  task "irb" => "rubyc" do
+    env = {
+      "ENCLOSE_IO_ALWAYS_USE_ORIGINAL_RUBY" => "1",
+      "ENCLOSE_IO_USE_ORIGINAL_RUBY"        => "1",
+    }
+
+    sh env, "./rubyc", "/__enclose_io_memfs__/bin/irb"
+  end
+end
+
 Rake::TestTask.new "test:unit" do |task|
+  task.pattern = "test/unit/test_*.rb"
   task.warning = true
 end
+
+Rake::TestTask.new "test:round-trip" do |task|
+  task.pattern = "test/round-trip/test_*.rb"
+  task.warning = true
+end
+
+task "test:round-trip" => "rubyc"
---
+++ test/round-trip/test_rubyc.rb
@@ -0,0 +1,64 @@
+require "minitest/autorun"
+require "open3"
+
+class TestRoundTrip < Minitest::Test
+  def setup
+    @rubyc = File.expand_path "../../../rubyc", __FILE__
+
+    assert File.exist?(@rubyc),
+           "could not find `rubyc` at #{@rubyc}, did you run `rake test:round-trip`?"
+
+    @env = {
+      "ENCLOSE_IO_ALWAYS_USE_ORIGINAL_RUBY" => "1",
+      "ENCLOSE_IO_USE_ORIGINAL_RUBY"        => "1",
+    }
+  end
+
+  def ruby(*args)
+    Open3.popen3(@env, @rubyc, *args) do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+
+      status = wait_thr.value
+
+      flunk "failed to run ruby #{args.join " "}" unless status.success?
+
+      return [stdout.read, stderr.read]
+    end
+  end
+
+  def rubyc(*args)
+    Open3.popen3(@rubyc, *args) do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+
+      status = wait_thr.value
+
+      flunk "failed to run rubyc #{args.join " "}" unless status.success?
+
+      return [stdout.read, stderr.read]
+    end
+  end
+
+  def test_help
+    out, = rubyc "--help"
+
+    assert_match "Compiling your Ruby application into a single executable.",
+                 out,
+                 "missing --help string"
+  end
+
+  def test_ruby_version
+    out, = rubyc "--ruby-version"
+
+    assert_match "2.5.1", out, "incorrect ruby version"
+  end
+
+  def test_unit_tests
+    out, err = ruby "/__enclose_io_memfs__/bin/rake", "test:unit"
+
+  #rescue
+    $stderr.puts "rake test stdout:"
+    $stderr.puts out
+    $stderr.puts "rake test stderr:"
+    $stderr.puts err
+  end
+end
