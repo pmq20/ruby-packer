f8a3c1e48a3f18c40011a15d2f41a27da2f9ddf7
Yousaf Nabi <you@saf.dev>
2023-08-12 06:12:40 +0100

    chore

--- patch_ruby_source.sh
+++ patch_ruby_source.sh
@@ -2,6 +2,7 @@
 rm -rf ruby
 ./download-ruby-source.sh
 mv ruby-3.2.2 ruby
-git status -s -uno | wc -l | grep -q 62 && echo "successful diff against ruby source" || echo "unexpected diff count" && exit 1
-find ruby-enclose -type f | wc -l | grep -q 62 && echo "successful count against enclose patch" || echo "not enough files to patch" && exit 1
+git status -s -uno | wc -l | grep -q 62 || exit 1
+find ruby-enclose -type f | grep -q 62 || exit 1
+find ruby-enclose -type f 
 cp -r ruby-enclose/* ruby
