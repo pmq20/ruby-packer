41b06e9abfe6eaba691514eba8ee8a35346c205f
Yousaf Nabi <you@saf.dev>
2023-08-12 05:20:46 +0100

    feat: extract enclose.io hacks

--- move_enclose_hacks_from_src.sh
+++ move_enclose_hacks_from_src.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
-mv ruby/dir.c ruby-enclose/dir.c
-mv ruby/dln.c ruby-enclose/dln.c
-mv ruby/file.c ruby-enclose/file.c
-mv ruby/io.c ruby-enclose/io.c
-mv ruby/main.c ruby-enclose/main.c
-mv ruby/process.c ruby-enclose/process.c
-mv ruby/ruby.c ruby-enclose/ruby.c
-mv ruby/util.c ruby-enclose/util.c
-mv ruby/lib/rubygems/path_support.rb ruby-enclose/lib/rubygems/path_support.rb
-mv ruby/tool/mkconfig.rb ruby-enclose/tool/mkconfig.rb
-mv ruby/win32/file.c ruby-enclose/win32/file.c
-mv ruby/win32/win32.c ruby-enclose/win32/win32.c
-mv vendor-enclose/gdbm/src/gdbmopen.c vendor/gdbm/src/gdbmopen.c
-mv ruby/enclose_io_common.h ruby-enclose/
-mv ruby/enclose_io_prelude.h ruby-enclose/
-mv ruby/enclose_io_unix.c ruby-enclose/
-mv ruby/enclose_io_unix.h ruby-enclose/
-mv ruby/enclose_io_win32.c ruby-enclose/
-mv ruby/enclose_io_win32.h ruby-enclose/
-mv ruby/enclose_io_winapi.h ruby-enclose/
-mv ruby/enclose_io.h ruby-enclose/
+mkdir -p ruby-enclose
+mkdir -p ruby-enclose/lib/rubygems
+mkdir -p ruby-enclose/win32
+mkdir -p ruby-enclose/tool
+
+## Patched ruby source files
+cp ruby/dir.c ruby-enclose/dir.c
+cp ruby/dln.c ruby-enclose/dln.c
+cp ruby/file.c ruby-enclose/file.c
+cp ruby/io.c ruby-enclose/io.c
+cp ruby/main.c ruby-enclose/main.c
+cp ruby/process.c ruby-enclose/process.c
+cp ruby/ruby.c ruby-enclose/ruby.c
+cp ruby/util.c ruby-enclose/util.c
+cp ruby/lib/rubygems/path_support.rb ruby-enclose/lib/rubygems/path_support.rb
+cp ruby/tool/mkconfig.rb ruby-enclose/tool/mkconfig.rb
+cp ruby/win32/file.c ruby-enclose/win32/file.c
+cp ruby/win32/win32.c ruby-enclose/win32/win32.c
+
+## Enclose specific files
+cp ruby/enclose_io_common.h ruby-enclose/
+cp ruby/enclose_io_prelude.h ruby-enclose/
+cp ruby/enclose_io_unix.c ruby-enclose/
+cp ruby/enclose_io_unix.h ruby-enclose/
+cp ruby/enclose_io_win32.c ruby-enclose/
+cp ruby/enclose_io_win32.h ruby-enclose/
+cp ruby/enclose_io_winapi.h ruby-enclose/
+cp ruby/enclose_io.h ruby-enclose/
+
+## Patched vendor files
+mkdir -p vendor-enclose/gdbm/src
+cp vendor/gdbm/src/gdbmopen.c vendor-enclose/gdbm/src/gdbmopen.c
---
+++ ruby-enclose/lib/rubygems/path_support.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+##
+#
+# Gem::PathSupport facilitates the GEM_HOME and GEM_PATH environment settings
+# to the rest of RubyGems.
+#
+class Gem::PathSupport
+  ##
+  # The default system path for managing Gems.
+  attr_reader :home
+
+  ##
+  # Array of paths to search for Gems.
+  attr_reader :path
+
+  ##
+  # Directory with spec cache
+  attr_reader :spec_cache_dir # :nodoc:
+
+  ##
+  #
+  # Constructor. Takes a single argument which is to be treated like a
+  # hashtable, or defaults to ENV, the system environment.
+  #
+  def initialize(env)
+    @home = env["GEM_HOME"] || Gem.default_dir
+
+    # --------- [Enclose.IO Hack start] ---------
+    # WE DO NOT ACCEPT OUTSIDE GEM PATHS
+    unless env['ENCLOSE_IO_RUBYC_1ST_PASS']
+      @home = Gem.default_dir unless 0 == @home.index('/__enclose_io_memfs__')
+    end
+    # --------- [Enclose.IO Hack end] ---------
+
+    if File::ALT_SEPARATOR
+      @home = @home.gsub(File::ALT_SEPARATOR, File::SEPARATOR)
+    end
+
+    @home = expand(@home)
+
+    @path = split_gem_path env["GEM_PATH"], @home
+
+    # --------- [Enclose.IO Hack start] ---------
+    # WE DO NOT ACCEPT OUTSIDE GEM PATHS
+    unless env['ENCLOSE_IO_RUBYC_1ST_PASS']
+      @path.keep_if do |x|
+        0 == x.index('/__enclose_io_memfs__')
+      end
+    end
+    # --------- [Enclose.IO Hack end] ---------
+    
+    @spec_cache_dir = env["GEM_SPEC_CACHE"] || Gem.default_spec_cache_dir
+
+    @spec_cache_dir = @spec_cache_dir.dup.tap(&Gem::UNTAINT)
+  end
+
+  private
+
+  ##
+  # Split the Gem search path (as reported by Gem.path).
+
+  def split_gem_path(gpaths, home)
+    # FIX: it should be [home, *path], not [*path, home]
+
+    gem_path = []
+
+    if gpaths
+      gem_path = gpaths.split(Gem.path_separator)
+      # Handle the path_separator being set to a regexp, which will cause
+      # end_with? to error
+      if gpaths =~ /#{Gem.path_separator}\z/
+        gem_path += default_path
+      end
+
+      if File::ALT_SEPARATOR
+        gem_path.map! do |this_path|
+          this_path.gsub File::ALT_SEPARATOR, File::SEPARATOR
+        end
+      end
+
+      gem_path << home
+    else
+      gem_path = default_path
+    end
+
+    gem_path.map {|path| expand(path) }.uniq
+  end
+
+  # Return the default Gem path
+  def default_path
+    Gem.default_path + [@home]
+  end
+
+  def expand(path)
+    if File.directory?(path)
+      File.realpath(path)
+    else
+      path
+    end
+  end
+end
